213738290b6ee2f269af7091b5916d28a5adb0b9
Ahmed Haitham Rehan <ah607k@gmail.com>
2025-12-08 13:37:26 +0400

    feat: configure better auth and add user model schema

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import { betterAuth } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prisma } from './prisma';
+import { nextCookies } from 'better-auth/next-js';
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  emailAndPassword: {
+    enabled: true,
+    minPasswordLength: 6,
+    requireEmailVerification: true,
+  },
+  plugins: [nextCookies()],
+  session: {
+    expiresIn: 60 * 60 * 24 * 30, // 30 days
+  },
+  user: {
+    additionalFields: {
+      role: {
+        type: 'string',
+        defaultValue: 'user',
+        input: false,
+      },
+    },
+  },
+});
